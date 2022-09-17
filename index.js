f27996a2c8366de1731ae48cc66dce6701f5f6d0
sanketpaul <sanketpaul7@gmail.com>
2022-09-17 20:20:24 +0530

    toggletext

---
+++ index.js
@@ -0,0 +1,8 @@
+let changetext=()=>{
+    if(document.getElementsByClassName('text')[0].innerText==='Sanket Paul'){
+document.getElementsByClassName('text')[0].innerText='I am one of the biggest fan of MSDHONI ❤'
+    }
+    else{
+        document.getElementsByClassName('text')[0].innerText='Sanket Paul'
+    }
+}
